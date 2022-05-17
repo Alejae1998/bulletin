3583a66cff3e2eed57a963c302e96e403b7ff982
Alejae1998 <alejagr1998@gmail.com>
2022-05-16 17:33:47 -0700

    add buttons and some css

--- app.js
+++ app.js
@@ -2,6 +2,17 @@
 
 // let state
 
+const logInForm = document.getElementById('log-in');
+const createForm = document.getElementById('create-button');
+
+logInForm.addEventListener('click', async => {
+
+})
+
+createForm.addEventListener('click', async => {
+  
+})
+
 // set event listeners 
   // get user input
   // use user input to update state 
